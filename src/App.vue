5abdd9b2c858f66f8a72fec4f7c856515180bc12
kkjhh0207 <kkjhh0207@gmail.com>
2023-01-24 22:01:13 +0900

    GUGUDAN

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
-  <div v-if="liked">좋아요 눌렀음</div>
-  <button v-else v-on:click="onClickButton">like</button>
+  <div>{{ first }} 곱하기 {{ second }} 는?</div>
+  <form v-on:submit="onSubmitForm">
+    <!--인풋과 데이터 value를 연결-->
+    <input type="number" v-model="value" />
+    <!--submit은 form을 제출-->
+    <button type="submit">입력</button>
+    <div id="result">{{ result }}</div>
+  </form>
 </template>
 
 <script>
@@ -9,12 +15,22 @@ export default {
   components: {},
   data() {
     return {
-      liked: false,
+      first: Math.ceil(Math.random() * 9),
+      second: Math.ceil(Math.random() * 9),
+      value: "",
+      result: "",
     };
   },
   methods: {
-    onClickButton() {
-      this.liked = true;
+    onSubmitForm(e) {
+      e.preventDefault(); //페이지 새로고침 방지
+      console.log(this.first);
+      console.log(this.second);
+      if (this.first * this.second == this.value) {
+        this.result = "정답";
+      } else {
+        this.result = "땡";
+      }
     },
   },
 };
