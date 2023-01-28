462e32824b971ca075ed6bfc93d56340bf06e771
kkjhh0207 <kkjhh0207@gmail.com>
2023-01-28 18:06:28 +0900

    word_chain_ref_focus

--- src/word_chain.vue
+++ src/word_chain.vue
@@ -2,7 +2,7 @@
   <div>끝말잇기 : {{ target }}</div>
   <form v-on:submit="onSubmitForm">
     <!--인풋과 데이터 value를 연결-->
-    <input type="text" v-model="value" />
+    <input type="text" ref="answer" v-model="value" />
     <!--submit은 form을 제출-->
     <button type="submit">입력</button>
     <div v-if="!target" id="result">시작 단어를 입력하세요</div>
@@ -29,15 +29,18 @@ export default {
       if (this.target == "") {
         this.target = this.value;
         this.value = "";
+        this.$refs.answer.focus();
       } else {
         console.log("target" + this.target[this.target.length - 1]);
         if (this.target[this.target.length - 1] == this.value[0]) {
           this.target = this.value;
           this.result = "정답";
           this.value = "";
+          this.$refs.answer.focus();
         } else {
           this.result = "오답";
           this.value = "";
+          this.$refs.answer.focus();
         }
       }
       console.log("1" + this.target);
